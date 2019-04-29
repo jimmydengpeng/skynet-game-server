f2214f97d6662742059fb941c8db279ba3f3f727
jimmydengpeng <121dengpeng@163.com>
2019-04-29 18:40:03 +0800

    tt

--- examples/mySocketListen.lua
+++ examples/mySocketListen.lua
@@ -5,7 +5,7 @@ skynet.start(function ( )
     local addr = '0.0.0.0'
     local port = 8001
     skynet.error("socket listen: "..addr..":"..port)
-    local lID = socket.listen(addr)
+    local lID = socket.listen(addr, port)
     assert(lID)
     socket.start(lID, function(cID, addr)
         skynet.error("socket accepted: "..addr)
